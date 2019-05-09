0ce3b6a2c8cf8058ece8695085e1bfd327bb0902
lee kang won <potato@zeroweb.kr>
2019-05-09 17:55:21 +0900

    Implement views
    Not fully worked yet.

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from app.models import Key, Translation
+from app.serializers import KeySerializer, TranslationSerializer
+
+
+class KeyViewSet(viewsets.GenericViewSet):
+    queryset = Key.objects.all()
+    serializer_class = KeySerializer
+
+    def list(self, request, *args, **kwargs):
+        return Response({
+            'keys': self.serializer_class(self.queryset.all(), many=True).data,
+        })
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'key': serializer.data,
+        })
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response({
+            'key': self.serializer_class(self.get_object()).data,
+        })
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+
+class TranslationViewSet(viewsets.ModelViewSet):
+    lookup_field = 'locale'
+    serializer_class = TranslationSerializer
+
+    def get_queryset(self):
+        key_id = self.kwargs.get('key_id')
+        return Translation.objects.filter(key_id=key_id)

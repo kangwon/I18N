637af0b826d213cd1b1689b24166fe9cfa82cb73
lee kang won <potato@zeroweb.kr>
2019-05-09 14:02:38 +0900

    Define Key, Translation models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+LOCALE_CHOICES = (
+    ('en', 'English'),
+    ('ko', 'Korean'),
+    ('ja', 'Japanese')
+)
+
+
+class Key(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+
+class Translation(models.Model):
+    id = models.AutoField(primary_key=True)
+    key_id = models.ForeignKey(
+        'Key',
+        related_name='translations',
+        on_delete=models.CASCADE,
+    )
+    locale = models.CharField(
+        max_length=2,
+        choices=LOCALE_CHOICES,
+    )
+    value = models.TextField()

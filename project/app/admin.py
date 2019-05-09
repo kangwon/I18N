96010b72d572712160cc3d775740ff73d04429d0
lee kang won <potato@zeroweb.kr>
2019-05-09 17:53:10 +0900

    Add admin

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Key, Translation
+
+
+@admin.register(Key)
+class KeyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = list_display
+
+
+@admin.register(Translation)
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'key_id', 'locale', 'value',)
+    list_display_links = list_display
